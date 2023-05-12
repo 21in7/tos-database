42499d3886b95e81073d5cc7e438c78df6bd0b72
21in7 <skawksmsduzo@gmail.com>
2023-05-12 11:52:03 +0900

    ktos url fix

--- tos-parser/src/constants.py
+++ tos-parser/src/constants.py
@@ -21,8 +21,8 @@ OUTPUT_SKILLS = 'skills'
 URL_PATCH = None
 URL_PATCH_iTOS = 'http://drygkhncipyq8.cloudfront.net/toslive/patch/'
 URL_PATCH_jTOS = 'http://d3bbj7hlpo9jjy.cloudfront.net/live/patch/'
-URL_PATCH_kTOS = 'http://tosg.dn.nexoncdn.co.kr/patch/live/'
-URL_PATCH_kTEST = 'http://tosg.dn.nexoncdn.co.kr/patch/test/'
+URL_PATCH_kTOS = 'http://d31k064uwo645x.cloudfront.net/patchkor/'
+URL_PATCH_kTEST = 'http://d23z5ksk9uuuh6.cloudfront.net/tostest/patchkor/'
 URL_PATCH_twTOS = 'http://tospatch.x2game.com.tw/live/patch/'
 
 PATH_INPUT = None
